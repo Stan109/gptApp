6b533dbca210bd3ac4d068b90650a7554116d0a3
stsek <stanislas.sekelj@smile.fr>
2024-12-05 15:09:05 +0100

    changes config.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 
-console.log("db user : ", process.env.DB_USER)
-console.log("db password : ", process.env.DB_PASSWORD)
-console.log("db name : ", process.env.DB_NAME)
-console.log("db host : ", process.env.DB_HOST)
-console.log("db port : ", process.env.DB_PORT)
+console.log("db user : ", process.env.DB_USER);
+console.log("db password : ", process.env.DB_PASSWORD);
+console.log("db name : ", process.env.DB_NAME);
+console.log("db host : ", process.env.DB_HOST);
+console.log("db port : ", process.env.DB_PORT);
 
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "db",
-    port: 5432,
-    dialect: 'postgres'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USER,
@@ -21,7 +21,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
+    dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USER,
@@ -29,6 +29,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
-  }
+    dialect: 'postgres',
+  },
 };
